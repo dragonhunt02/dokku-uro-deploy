695e52c78e3222835d497af2ed1d87167fdf254d
dragonhunt02 <jackhunt43@libero.it>
2025-05-03 05:11:03 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e;
 
+# Set website domain
+ROOT_URL="vsekai.local"
+
 echo "Deploy setup started!";
+echo "Domain: ${ROOT_URL}"
 
 # (Optional) Setup letsencrypt email for SSL
 # dokku traefik:set --global letsencrypt-email automated@example.com
@@ -15,7 +19,7 @@ echo "Deploy setup started!";
 # dokku traefik:set --global basic-auth-password password
 
 # (Optional) Set traefik dashboard hostname
-# dokku traefik:set --global api-vhost traefik.dokku.local
+# dokku traefik:set --global api-vhost traefik.vsekai.local
 
 # Start traefik
 dokku proxy:set --global traefik;
@@ -28,9 +32,9 @@ dokku plugin:install https://github.com/dokku/dokku-redis.git --name redis;
 dokku plugin:install https://github.com/dragonhunt02/dokku-uro-deploy.git;
 
 # Setup backend env
-SETUP='ROOT_ORIGIN=https://vsekai.local
-URL=https://vsekai.local/api/v1/
-FRONTEND_URL=https://vsekai.local/
+SETUP="ROOT_ORIGIN=https://${ROOT_URL}
+URL=https://${ROOT_URL}/api/v1/
+FRONTEND_URL=https://${ROOT_URL}/
 REDIS_URL=redis://redis:6379
 PHOENIX_KEY_BASE=bNDe+pg86uL938fQA8QGYCJ4V7fE5RAxoQ8grq9drPpO7mZ0oEMSNapKLiA48smR
 JOKEN_SIGNER=gqawCOER09ZZjaN8W2QM9XT9BeJSZ9qc
@@ -42,7 +46,7 @@ OAUTH2_GITHUB_CLIENT_SECRET=
 OAUTH2_DISCORD_STRATEGY=Assent.Strategy.Discord
 OAUTH2_DISCORD_CLIENT_ID=
 OAUTH2_DISCORD_CLIENT_SECRET=
-PORT=4000';
+PORT=4000";
 
 SETUP="$(echo "$SETUP" | tr "\n" " ")";
 
@@ -66,7 +70,8 @@ dokku builder:set nodeapp build-dir frontend;
 dokku docker-options:add nodeapp build "--build-arg uro_image=dokku/uroapp";
 
 # Required for traefik
-dokku domains:add nodeapp uroapp.dokku.local
+dokku domains:add uroapp ${ROOT_URL}
+dokku domains:add nodeapp ${ROOT_URL}
 
 # Frontend requires a network to talk to backend. Url is API_ORIGIN frontend env variable
 dokku network:create uronet
@@ -74,10 +79,10 @@ dokku network:set uroapp attach-post-deploy uronet
 dokku network:set nodeapp attach-post-deploy uronet
 
 # Setup frontend env
-SETUP2='NEXT_PUBLIC_ORIGIN=https://vsekai.local
+SETUP2="NEXT_PUBLIC_ORIGIN=https://${ROOT_URL}
 API_ORIGIN=http://uroapp.web:5000
-NEXT_PUBLIC_API_ORIGIN=https://vsekai.local/api/v1
-NEXT_PUBLIC_TURNSTILE_SITEKEY=1x00000000000000000000AA';
+NEXT_PUBLIC_API_ORIGIN=https://${ROOT_URL}/api/v1
+NEXT_PUBLIC_TURNSTILE_SITEKEY=1x00000000000000000000AA";
 
 SETUP2="$(echo "$SETUP2" | tr "\n" " ")";
 dokku config:set nodeapp $SETUP2;
